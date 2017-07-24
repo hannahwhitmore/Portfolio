79cbed4ed33be66238b0c6433da728dfbf061118
hannahwhitmore <hwhitmore19@gmail.com>
2017-07-24 14:13:59 -0400

    Add files via upload

---
+++ changecolors.ino
@@ -0,0 +1,36 @@
+int red = 13;
+int yellow = 12;
+int green = 9;
+
+void setup() {
+pinMode(red, OUTPUT);
+pinMode(yellow, OUTPUT);
+pinMode(green, OUTPUT);
+}
+
+void loop() {
+changeLights();
+delay(500);
+
+}
+void changeLights(){
+  digitalWrite(green, LOW);
+  digitalWrite(yellow, HIGH);
+  digitalWrite(red, LOW);
+  delay(500);
+
+  digitalWrite(yellow, LOW);
+  digitalWrite(red, HIGH);
+  digitalWrite(green, LOW);
+  delay(500);
+
+
+  digitalWrite(yellow, LOW);
+  digitalWrite(red, LOW);
+  digitalWrite(green, HIGH);
+  delay(500);
+  
+}
+
+
+

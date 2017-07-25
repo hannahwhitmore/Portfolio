a5e3657f88a4aeed53a929c6a3efff9c538e4e35
hannahwhitmore <hwhitmore19@gmail.com>
2017-07-25 09:43:50 -0400

    Add files via upload

---
+++ switchblink.ino
@@ -0,0 +1,22 @@
+int [] outputs = [0,1,2]
+void setup() {
+  pinMode(0, OUTPUT);
+  pinMode(1, OUTPUT);
+  pinMode(2, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(0, HIGH);
+  delay(100);
+  digitalWrite(0, LOW);
+  delay(100);
+  digitalWrite(1, HIGH);
+  delay(100);
+  digitalWrite(1, LOW);
+  delay(100);
+  digitalWrite(2, HIGH);
+  delay(100);
+  digitalWrite(2, LOW);
+  delay(100);
+
+}
